121a77c2971ef82cbab9db523fc84b52d5b00285
Tomasz Makarewicz <btchtm@ug.gda.pl>
2013-09-02 11:43:10 +0200

    Update e-mail

--- dynamics_pymol_plugin.py
+++ dynamics_pymol_plugin.py
@@ -4,7 +4,7 @@
 ##This software (including its Debian packaging) is available to you under the terms of the GPL-3, see "/usr/share/common-licenses/GPL-3".
 ##Software is created and maintained by Laboratory of Biomolecular Systems Simulation at University of Gdansk.
 ##Contributors:
-##- Tomasz Makarewicz (tomaszm@biotech.ug.edu.pl)
+##- Tomasz Makarewicz (btchtm@ug.gda.pl)
 
 ##Plugin Version
 plugin_ver = " 2.0.0pre"
