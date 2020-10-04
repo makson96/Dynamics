1f9d000e6a1242221bc1aba79711a6fbec75a1e7
Tomasz Makarewicz <tomasz.makarewicz@protonmail.ch>
2020-10-04 16:23:24 +0200

    Add some more unit tests.

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
-from pymol_plugin_dynamics import get_dynamics_dir
 import os
+from pymol_plugin_dynamics import get_dynamics_dir, get_project_dirs, standardize_new_line_char
 
 
 class Test(TestCase):
@@ -8,3 +8,32 @@ class Test(TestCase):
         exp = "/home/{}/.dynamics/".format(os.getenv("USER"))
         obs = get_dynamics_dir()
         self.assertEqual(exp, obs)
+
+    def test_get_project_dirs(self):
+        value1 = "abcdefg"
+        exp = "/home/{}/.dynamics/{}/".format(os.getenv("USER"), value1)
+        obs = get_project_dirs(value1)
+        self.assertEqual(exp, obs)
+
+    def test_standardize_new_line_char1(self):
+        exp = "aaa"
+        obs = standardize_new_line_char("aaa")
+        self.assertEqual(exp, obs)
+
+    def test_standardize_new_line_char2(self):
+        exp = "aaa\nbbb"
+        obs = standardize_new_line_char("aaa\nbbb")
+        self.assertEqual(exp, obs)
+
+    def test_standardize_new_line_char3(self):
+        exp = "aaa\nbbb\nccc"
+        obs = standardize_new_line_char("aaa\rbbb\r\nccc")
+        self.assertEqual(exp, obs)
+
+
+if __name__ == "__main__":
+    u_tests = Test()
+    u_tests.test_get_dynamics_dir()
+    u_tests.test_get_project_dirs()
+    u_tests.test_standardize_new_line_char1()
+    u_tests.test_standardize_new_line_char2()
